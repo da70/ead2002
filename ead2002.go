18006860a3461504e8708a99e3adafdc10084d05
R. S. Doiel <rsdoiel@gmail.com>
2016-06-01 15:05:19 -0700

    Started testing Parse and generated data structures

--- ead2002.go
+++ ead2002.go
@@ -34,9 +34,15 @@
 package ead2002
 
 import (
+	"encoding/json"
 	"encoding/xml"
-	"time"
 )
+
+type EAD struct {
+	XMLName   xml.Name   `json:"-"`
+	EADHeader *EADHeader `xml:"eadheader,omitempty" json:"eadheader,omitempty"`
+	ArchDesc  *ArchDesc  `xml:"archdesc,omitempty" json:"archdesc,omitempty"`
+}
 
 type EADHeader struct {
 	XMLName            xml.Name     `json:"-"`
@@ -57,7 +63,9 @@ type EADID struct {
 	CountryCode    string   `xml:"countrycode,attr,omitempty" json:"countrycode,omitempty"`
 	Identifier     string   `xml:"identifier,attr,omitempty" json:"identifier,omitempty"`
 	MainAgencyCode string   `xml:"mainagencycode,attr,omitempty" json:"mainagencycode,omitempty"`
-	Value          string   `xml:",innerxml,omitempty" json:"value,omitempty"`
+	PublicID       string   `xml:"publicid,attr,omitempty" json:"publicid,omitempty"`
+	PathParent     string   `xml:"path:parent,attr,omitempty" json:"path_parent,omitempty"`
+	Value          string   `xml:",innerxml" json:"value"`
 }
 
 type FileDesc struct {
@@ -67,41 +75,53 @@ type FileDesc struct {
 }
 
 type TitleStmt struct {
-	XMLName     xml.Name     `json:"-"`
-	TitleProper *TitleProper `xml:"titleproper,omitempty" json:"titleproper,omitempty"`
-	Subtitle    string       `xml:"subtitle,omitempty" json:"subtitle,omitempty"`
-	Author      *Author      `xml:"author,omitempty" json:"author,omitempty"`
+	XMLName     xml.Name      `json:"-"`
+	TitleProper []TitleProper `xml:"titleproper,omitempty" json:"titleproper,omitempty"`
+	//Subtitle    string        `xml:"subtitle,omitempty" json:"subtitle,omitempty"`
+	Author *Author `xml:"author,omitempty" json:"author,omitempty"`
 }
 
 type TitleProper struct {
 	XMLName        xml.Name `json:"-"`
 	EncodingAnalog string   `xml:"encodinganalog,attr,omitempty" json:"encodinganalog,omitempty"`
-	Value          string   `xml:",innerxml,omitempty" json:"value,omitempty"`
+	Type           string   `xml:"type,attr,omitempty" json:"type,omitempty"`
+	Value          string   `xml:",innerxml" json:"value"`
 }
 
 type Author struct {
 	XMLName        xml.Name `json:"-"`
 	EncodingAnalog string   `xml:"encodinganalog,attr,omitempty" json:"encodinganalog,omitempty"`
-	Value          string   `xml:",innerxml,omitempty" json:"value,omitempty"`
+	Value          string   `xml:",chardata" json:"value"`
 }
 
 type PublicationStmt struct {
-	XMLName   xml.Name   `json:"-"`
-	Publisher *Publisher `xml:"publisher,omitempty" json:"publisher,omitempty"`
-	Date      *time.Time `xml:"date,omitempty" json:"date,omitempty"`
+	XMLName   xml.Name      `json:"-"`
+	Publisher *Publisher    `xml:"publisher,omitempty" json:"publisher,omitempty"`
+	P         *P            `xml:"p,omitempty" json:"p,omitempty"`
+	Address   *AddressLines `xml:"address,omitempty" json:"address,omitempty"`
+	Date      string        `xml:"date,omitempty" json:"date,omitempty"`
+}
+
+type P struct {
+	XMLName xml.Name `json:"-"`
+	Value   string   `xml:",innerxml" json:"value,omitempty"`
+}
+
+type AddressLines struct {
+	AddressLine []string `xml:"addressline,omitempty" json:"addressline,omitempty"`
 }
 
 type Data struct {
 	XMLName        xml.Name `json:"-"`
 	EncodingAnalog string   `xml:"encodinganalog,attr,omitempty" json:"encodinganalog,omitempty"`
 	Normal         string   `xml:"normal,attr,omitempty" json:"normal,omitempty"`
-	Value          string   `xml:",innerxml,omitempty" json:"value,omitempty"`
+	Value          string   `xml:",innerxml" json:"value"`
 }
 
 type Publisher struct {
 	XMLName        xml.Name `json:"-"`
 	EncodingAnalog string   `xml:"encodinganalog,attr,omitempty" json:"encodinganalog,omitempty"`
-	Value          string   `xml:",innerxml,omitempty" json:"value,omitempty"`
+	Value          string   `xml:",innerxml" json:"value,omitempty"`
 }
 
 type ProfileDesc struct {
@@ -112,7 +132,7 @@ type ProfileDesc struct {
 
 type Creation struct {
 	XMLName xml.Name `json:"-"`
-	Value   string   `xml:",innerxml,omitempty" json:"value,omitempty"`
+	Value   string   `xml:",innerxml" json:"value,omitempty"`
 }
 
 type LangUsage struct {
@@ -124,24 +144,32 @@ type Language struct {
 	XMLName        xml.Name `json:"-"`
 	EncodingAnalog string   `xml:"encodinganalog,attr,omitempty" json:"encodinganalog,omitempty"`
 	LangCode       string   `xml:"langcode,attr,omitempty" json:"langcode,omitempty"`
-	Value          string   `xml:",innerxml,omitempty" json:"value,omitempty"`
+	Value          string   `xml:",innerxml" json:"value,omitempty"`
 }
 
 type LanguageList []*Language
 
 type ArchDesc struct {
-	XMLName         xml.Name `json:"-"`
-	Level           string   `xml:"level,attr,omitempty" json:"level,omitempty"`
-	Type            string   `xml:"type,attr,omitempty" json:"type,omitempty"`
-	RelatedEncoding `xml:"relatedencoding,attr,omitempty" json:"relatedencoding,omitempty"`
-	DID             *DID `xml:"did,omitempty" json:"did,omitempty"`
+	XMLName         xml.Name        `json:"-"`
+	Level           string          `xml:"level,attr,omitempty" json:"level,omitempty"`
+	Type            string          `xml:"type,attr,omitempty" json:"type,omitempty"`
+	RelatedEncoding string          `xml:"relatedencoding,attr,omitempty" json:"relatedencoding,omitempty"`
+	DID             []DID           `xml:"did,omitempty" json:"did,omitempty"`
+	AccessRestrict  *AccessRestrict `xml:"accessrestrict,omitempty" json:"accessrestrict,omitempty"`
+	UserRestrict    *UserRestrict   `xml:"userrestrict,omitempty" json:"userrestrict,omitempty"`
+	Prefercite      *Prefercite     `xml:"prefercite,omitempty" json:"prefercite,omitempty"`
+	AcqInfo         *AcqInfo        `xml:"acqinfo,omitempty" json:"acqinfo,omitempty"`
+	BiogHist        *BiogHist       `xml:"bioghist,omitempty" json:"bioghist,omitempty"`
+	ScopeContent    *ScopeContent   `xml:"scopecontent,omitempty" json:"scopecontent,omitempty"`
+	ControlAccess   *ControlAccess  `xml:"controlaccess,omitempty" json:"controlaccess,omitempty"`
+	OtherFindAid    *OtherFindAid   `xml:"otherfindaid,omitempty" json:"otherfindaid,omitempty"`
 }
 
 type DID struct {
 	XMLName      xml.Name      `json:"-"`
 	Head         string        `xml:"head,omitempty" json:"omitempty"`
 	Repository   *Repository   `xml:"repository,omitempty" json:"repository,omitempty"`
-	Organization *Organization `xml:"organization,omitempty" json:"organization,omitempty"`
+	Origination  *Origination  `xml:"origination,omitempty" json:"origination,omitempty"`
 	UnitTitle    *UnitTitle    `xml:"unittitle,omitempty" json:"unittitle,omitempty"`
 	UnitDate     *UnitDate     `xml:"unitdate,omitempty" json:"unitdate,omitempty"`
 	PhysDesc     *PhysDesc     `xml:"physdesc,omitempty" json:"physdesc,omitempty"`
@@ -150,23 +178,48 @@ type DID struct {
 	LangMaterial *LangMaterial `xml:"langmaterial,omitempty" json:"langmaterial,omitempty"`
 }
 
-type Organization struct {
+type LangMaterial struct {
 	XMLName  xml.Name  `json:"-"`
-	Label    string    `xml:"label,attr,omitempty" json:"label,omitempty"`
-	PersName *PersName `xml:"persname,omitempty" json:"persname,omitempty"`
+	Language *Language `xml:"language,omitempty" json:"language,omitempty"`
 }
 
-type PersName struct {
+type UnitID struct {
+	XMLName xml.Name `json:"-"`
+	Value   string   `xml:",innerxml" json:"value,omitempty"`
+}
+
+type Abstract struct {
+	XMLName xml.Name `json:"-"`
+	Label   string   `xml:"label,attr,omitempty" json:"label,omitempty"`
+	Value   string   `xml:",innerxml" json:"value,omitempty"`
+}
+
+type Origination struct {
+	XMLName  xml.Name  `json:"-"`
+	Label    string    `xml:"label,attr,omitempty" json:"label,omitempty"`
+	Persname *Persname `xml:"persname,omitempty" json:"persname,omitempty"`
+	Famname  *Famname  `xml:"famname,omitempty" json:"famname,omitempty"`
+}
+
+type Persname struct {
 	XMLName        xml.Name `json:"-"`
 	EncodingAnalog string   `xml:"encodinganalog,attr,omitempty" json:"encodinganalog,omitempty"`
-	Value          string   `xml:",innerxml,omitempty" json:"value,omitempty"`
+	Source         string   `xml:"source,attr,omitempty" json:"source,omitempty"`
+	Value          string   `xml:",innerxml" json:"value,omitempty"`
+}
+
+type Famname struct {
+	XMLName        xml.Name `json:"-"`
+	EncodingAnalog string   `xml:"encodinganalog,attr,omitempty" json:"encodinganalog,omitempty"`
+	Source         string   `xml:"source,attr,omitempty" json:"source,omitempty"`
+	Value          string   `xml:",innerxml" json:"value,omitempty"`
 }
 
 type UnitTitle struct {
 	XMLName        xml.Name `json:"-"`
 	Label          string   `xml:"label,attr,omitempty" json:"label,omitempty"`
 	EncodingAnalog string   `xml:"encodinganalog,attr,omitempty" json:"encodinganalog,omitempty"`
-	Value          string   `xml:",innerxml,omitempty" json:"value,omitempty"`
+	Value          string   `xml:",innerxml" json:"value,omitempty"`
 }
 
 type UnitDate struct {
@@ -175,7 +228,7 @@ type UnitDate struct {
 	Type           string   `xml:"type,attr,omitempty" json:"type,omitempty"`
 	Label          string   `xml:"label,attr,omitempty" json:"label,omitempty"`
 	EncodingAnalog string   `xml:"encodinganalog,attr,omitempty" json:"encodinganalog,omitempty"`
-	Value          string   `xml:",innerxml,omitempty" json:"value,omitempty"`
+	Value          string   `xml:",innerxml" json:"value,omitempty"`
 }
 
 type PhysDesc struct {
@@ -186,6 +239,117 @@ type PhysDesc struct {
 }
 
 type Extent struct {
+	XMLName   xml.Name `json:"-"`
+	AltRender string   `xml:"altrender,attr,omitempty" json:"altrender,omitempty"`
+	Value     string   `xml:",innerxml" json:"value,omitempty"`
 }
 
 type ExtentList []Extent
+
+type AccessRestrict struct {
+	XMLName xml.Name `json:"-"`
+	ID      string   `xml:"id,attr,omitempty" json:"id,omitempty"`
+	Head    string   `xml:"head,omitemtpy" json:"head,omitempty"`
+	P       *P       `xml:"p,omitempty" json:"p,omitempty"`
+}
+
+type UserRestrict struct {
+	XMLName xml.Name `json:"-"`
+	ID      string   `xml:"id,attr,omitempty" json:"id,omitempty"`
+	Head    string   `xml:"head,omitemtpy" json:"head,omitempty"`
+	P       *P       `xml:"p,omitempty" json:"p,omitempty"`
+}
+
+type Prefercite struct {
+	XMLName xml.Name `json:"-"`
+	ID      string   `xml:"id,attr,omitempty" json:"id,omitempty"`
+	Head    string   `xml:"head,omitemtpy" json:"head,omitempty"`
+	P       *P       `xml:"p,omitempty" json:"p,omitempty"`
+}
+
+type AcqInfo struct {
+	XMLName xml.Name `json:"-"`
+	ID      string   `xml:"id,attr,omitempty" json:"id,omitempty"`
+	Head    string   `xml:"head,omitemtpy" json:"head,omitempty"`
+	P       *P       `xml:"p,omitempty" json:"p,omitempty"`
+}
+
+type BiogHist struct {
+	XMLName xml.Name `json:"-"`
+	ID      string   `xml:"id,attr,omitempty" json:"id,omitempty"`
+	Head    string   `xml:"head,omitemtpy" json:"head,omitempty"`
+	P       *P       `xml:"p,omitempty" json:"p,omitempty"`
+}
+
+type ScopeContent struct {
+	XMLName xml.Name `json:"-"`
+	ID      string   `xml:"id,attr,omitempty" json:"id,omitempty"`
+	Head    string   `xml:"head,omitemtpy" json:"head,omitempty"`
+	P       *P       `xml:"p,omitempty" json:"p,omitempty"`
+}
+
+type OtherFindAid struct {
+	XMLName xml.Name `json:"-"`
+	ID      string   `xml:"id,attr,omitempty" json:"id,omitempty"`
+	Head    string   `xml:"head,omitemtpy" json:"head,omitempty"`
+	List    []List   `xml:"list,omitempty" json:"list,omitempty"`
+}
+
+type List struct {
+	XMLName xml.Name `json:"-"`
+	Item    []Item   `xml:"item,omitempty" json:"item,omitempty"`
+}
+
+type Item struct {
+	XMLName xml.Name `json:"-"`
+	ExtRef  *ExtRef  `xml:"extref,omitempty" json:"extref,omitempty"`
+}
+
+type ExtRef struct {
+	XMLName xml.Name `json:"-"`
+	HRef    string   `xml:"href,attr,omitempty" json:"href,omitempty"`
+	Value   string   `xml:",innerxml" json:"value,omitempty"`
+}
+
+type ControlAccess struct {
+	XMLName  xml.Name  `json:"-"`
+	Subject  []Subject `xml:"subject,omitempty" json:"subject,omitempty"`
+	Corpname *Corpname `xml:"corpname,omitempty" json:"corpname,omitempty"`
+}
+
+type Subject struct {
+	XMLName xml.Name `json:"-"`
+	Source  string   `xml:"source,attr,omitempty" json:"source,omitempty"`
+	Value   string   `xml:",innerxml" json:"value,omitempty"`
+}
+
+type Corpname struct {
+	XMLName xml.Name `json:"-"`
+	Source  string   `xml:"source,attr,omitempty" json:"source,omitempty"`
+	Value   string   `xml:",innerxml" json:"value,omitempty"`
+}
+
+type Repository struct {
+	XMLName  xml.Name      `json:"-"`
+	Corpname *Corpname     `xml:"corpname,omitempty" json:"corpname,omitempty"`
+	Address  *AddressLines `xml:"address,omitempty" json:"address,omitempty"`
+}
+
+// Parse parses a ead2002 XML file into a set of Go structures
+func Parse(src []byte) (*EAD, error) {
+	ead := new(EAD)
+	err := xml.Unmarshal(src, ead)
+	return ead, err
+}
+
+// String write the EAD structure as JSON
+func (ead *EAD) String() string {
+	src, _ := json.Marshal(ead)
+	return string(src)
+}
+
+// ToXML write the EAD structure as XML
+func (ead *EAD) ToXML() []byte {
+	src, _ := xml.Marshal(ead)
+	return src
+}
--- ead2002_test.go
+++ ead2002_test.go
@@ -34,9 +34,208 @@
 package ead2002
 
 import (
+	"io/ioutil"
+	"path"
+	"strings"
 	"testing"
 )
 
 func TestBasic(t *testing.T) {
-	t.Errorf("ead2002 not implemented")
+	// See if we can read an EAD produced by ArchivesSpace
+	testParse10021_MS := func(fname string) {
+		src, err := ioutil.ReadFile(fname)
+		if err != nil {
+			t.Errorf("Could not read %s", fname)
+		}
+		ead, err := Parse(src)
+		if err != nil {
+			t.Errorf("Could not parse %s, %s\n", fname, err)
+			t.FailNow()
+		}
+		if ead == nil {
+			t.Errorf("Expected a populated ead")
+			t.FailNow()
+		}
+		if ead.EADHeader == nil {
+			t.Errorf("Expected a populated ead.EADHeader")
+			t.FailNow()
+		}
+		if ead.EADHeader.EADID == nil {
+			t.Errorf("Expected a populated ead.EADHeader.EADID")
+			t.FailNow()
+		}
+		if ead.EADHeader.EADID.Value != "10021-MS" {
+			t.Errorf("Missing value for EADID")
+		}
+		if ead.EADHeader.FileDesc == nil {
+			t.Errorf("Expected a populated ead.EADHeader.FileDesc")
+			t.FailNow()
+		}
+		if ead.EADHeader.FileDesc.TitleStmt == nil {
+			t.Errorf("Expected a populated ead.EADHeader.FileDesc.TitleStmt")
+			t.FailNow()
+		}
+		if len(ead.EADHeader.FileDesc.TitleStmt.TitleProper) != 2 {
+			t.Errorf("Expected a populated ead.EADHeader.FileDesc.TitleStmt.TitleProper array")
+			t.FailNow()
+		}
+		// This array is not ordered so adjust sequence check as necessary
+		a, b := 0, 1
+		tp := ead.EADHeader.FileDesc.TitleStmt.TitleProper
+		if tp[0].Type == "" {
+			a = 1
+			b = 0
+		}
+		if tp[a].Type != "filing" {
+			t.Errorf("Wrong value for TitleProper[0].Type: %+v", tp[a])
+		}
+		if tp[a].Value != "Millikan (Clark, B.) Papers" {
+			t.Errorf("Wrong value for TitleProper[0].Value: %+v", tp[a])
+		}
+		if tp[b].Value == "" {
+			t.Errorf("Wrong value for TitleProper[1].Value %+v", tp[b])
+		}
+		if ead.EADHeader.FileDesc.TitleStmt.Author == nil {
+			t.Errorf("Expected a populated ead.EADHeader.FileDesc.Author")
+			t.FailNow()
+		}
+		expected := "Finding aid created by California Institute of Technology staff using RecordEXPRESS"
+		if ead.EADHeader.FileDesc.TitleStmt.Author.Value != expected {
+			t.Errorf("Expected a populated Author value: %+v", ead.EADHeader.FileDesc.TitleStmt.Author)
+		}
+		if ead.EADHeader.FileDesc.PublicationStmt == nil {
+			t.Errorf("Expected a populated ead.EADHeader.FileDesc.PublicationStmt")
+			t.FailNow()
+		}
+		if ead.EADHeader.FileDesc.PublicationStmt.Publisher == nil {
+			t.Errorf("Expected a populated ead.EADHeader.FileDesc.PublicationStmt.Publisher")
+			t.FailNow()
+		}
+		if strings.HasSuffix(fname, "OAC.xml") {
+			if ead.EADHeader.FileDesc.PublicationStmt.Address == nil {
+				t.Errorf("Missing FileDesc.PublicationStmt.Address")
+				t.FailNow()
+			}
+			expected = "California Institute of Technology"
+			if ead.EADHeader.FileDesc.PublicationStmt.Publisher.Value != expected {
+				t.Errorf("Wrong value for ead.EADHeader.FileDesc.PublicationStmt.Publisher.Value: %+v", ead.EADHeader.FileDesc.PublicationStmt.Publisher.Value)
+			}
+			if len(ead.EADHeader.FileDesc.PublicationStmt.Address.AddressLine) != 5 {
+				t.Errorf("Wrong value ead.EADHeader.FileDesc.PublicationStmt.Address, %+v", ead.EADHeader.FileDesc.PublicationStmt)
+			}
+		} else {
+			if ead.EADHeader.FileDesc.PublicationStmt.Publisher.Value != "Caltech Archives" {
+				t.Errorf("Wrong value for FileDesc.PublicationStmt.Publisher.Value: %+v", ead.EADHeader.FileDesc.PublicationStmt.Publisher.Value)
+			}
+			if ead.EADHeader.FileDesc.PublicationStmt.P == nil {
+				t.Errorf("Expected a populated ead.EADHeader.FileDesc.PublicationStmt.P")
+				t.FailNow()
+			}
+			if ead.EADHeader.FileDesc.PublicationStmt.P.Value != `<date>2014</date>` {
+				t.Errorf("Wrong value for ad.EADHeader.FileDesc.PublicationStmt.P, %+v", ead.EADHeader.FileDesc.PublicationStmt.P)
+			}
+
+			if ead.EADHeader.ProfileDesc == nil {
+				t.Errorf("Expected a populated ead.EADHeader.ProfileDesc")
+				t.FailNow()
+			}
+			profileDesc := ead.EADHeader.ProfileDesc
+			if profileDesc.Creation == nil {
+				t.Errorf("Expected a populated profileDesc.Creation")
+				t.FailNow()
+			}
+			expected = "This finding aid was produced using ArchivesSpace on <date>2016-06-01 16:42:46 UTC</date>."
+			if strings.Compare(profileDesc.Creation.Value, expected) != 0 {
+				t.Errorf("Wrong value for profileDesc.Creation, %+v", profileDesc.Creation)
+			}
+		}
+		if ead.ArchDesc == nil {
+			t.Errorf("Expected a populated ead.ArchDesc")
+			t.FailNow()
+		}
+		if len(ead.ArchDesc.DID) != 1 {
+			t.Errorf("Wrong value for archDesc.DID")
+			t.FailNow()
+		}
+		archDesc := ead.ArchDesc
+		dids := archDesc.DID
+		did := dids[0]
+
+		if did.LangMaterial == nil {
+			t.Errorf("Expected populated did")
+			t.FailNow()
+		}
+		if did.LangMaterial.Language == nil {
+			t.Errorf("Missing did.LangMaterial.Language")
+		}
+		if strings.HasSuffix(fname, "AS.xml") {
+			if did.LangMaterial.Language.Value != "English" {
+				t.Errorf("Wrong value for did.LangMaterial, %+v", did.LangMaterial)
+			}
+			if did.LangMaterial.Language.LangCode != "eng" {
+				t.Errorf("Wrong value for did.LangMaterial.LangCode, %+v", did.LangMaterial)
+			}
+		} else {
+			if did.LangMaterial.Language.LangCode != "eng" {
+				t.Errorf("Wrong value for did.LangMaterial.LangCode, %+v", did.LangMaterial)
+			}
+		}
+		if did.Repository == nil {
+			t.Errorf("Expected a populated did.Repository")
+			t.FailNow()
+		}
+		if did.Repository.Corpname == nil {
+			t.Errorf("Missing did.Repository.Corpname")
+		}
+		if did.Repository.Corpname.Value != "Caltech Archives" &&
+			did.Repository.Corpname.Value != "California Institute of Technology" {
+			t.Errorf("Wrong value for did.Repository.Corpname.Value, %+v", did.Repository.Corpname)
+		}
+		if strings.HasSuffix(fname, "OAC.xml") {
+			if did.Repository.Address == nil {
+				t.Errorf("Missing value for did.Repository.Address")
+			}
+			if len(did.Repository.Address.AddressLine) != 1 {
+				t.Errorf("Wrong value for did.Repository.Address.AddressLine, %+v", did.Repository.Address)
+			}
+		}
+		if did.Origination == nil {
+			t.Errorf("Expected populated did.Origination")
+			t.FailNow()
+		}
+		if did.Origination.Persname == nil {
+			t.Errorf("Missing did.Origination.Persname, %+v", did.Origination)
+		}
+		if did.Origination.Persname.Value != "Clark, B." {
+			t.Errorf("Wrong value for did.Origination.Persname.Value, %+v", did.Origination.Persname)
+		}
+		if strings.HasSuffix(fname, "AS.xml") {
+			if did.Origination.Persname.Source != "ingest" {
+				t.Errorf("Wrong value for did.Origination.Persname.Source, %+v", did.Origination.Persname)
+			}
+		} else {
+			if did.Origination.Famname.Value != "Millikan" {
+				t.Errorf("Wrong value for did.Origination.Famname, %+v", did.Origination)
+			}
+		}
+		if did.UnitTitle == nil {
+			t.Errorf("Expected a populated did.UnitTitle")
+			t.FailNow()
+		}
+		expected = "Guide to the Papers of Clark B. Millikan, 1922-1965"
+		if did.UnitTitle.Value != expected {
+			t.Errorf("Wrong value for did.UnitTitle.Value, %+v", did.UnitTitle)
+		}
+
+	}
+
+	//
+	// Test the ArchivesSpace generated EAD
+	//
+	testParse10021_MS(path.Join("example-eads", "10021-MS_ead-from-AS.xml"))
+
+	//
+	// Test the OAC version of the EAD
+	//
+	testParse10021_MS(path.Join("example-eads", "10021-MS_ead-from-OAC.xml"))
 }
